dc4d1b01c2a3d895b9e52d8ef6d8f841ea1db1a8
Furkan Gundogan <furkangundogan14@outlook.com>
2024-09-08 10:56:18 +0300

    GetAllAsync tests added

---
+++ src/Users.Api/Logging/LoggerAdapter.cs
@@ -0,0 +1,16 @@
+﻿namespace Users.Api.Logging
+{
+    public class LoggerAdapter<TType> : ILoggerAdapter<TType>
+    {
+        private readonly ILogger<TType> _logger;
+        public void LogError(Exception? exception, string? message, params object?[] args)
+        {
+            _logger.LogError(exception, message, args);
+        }
+
+        public void LogInformation(string? message, params object?[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+    }
+}
--- test/Users.Api.Tests.Unit/UserServiceTests.cs
+++ test/Users.Api.Tests.Unit/UserServiceTests.cs
@@ -1,11 +1,13 @@
 ﻿using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Users.Api.Logging;
 using Users.Api.Models;
 using Users.Api.Repositories;
 using Users.Api.Services;
@@ -16,7 +18,7 @@ namespace Users.Api.Tests.Unit
     {
         private readonly UserService _sut;
         private readonly IUserRepository _userRepository = Substitute.For<IUserRepository>();
-        private readonly ILogger<User> _logger = Substitute.For<ILogger<User>>();
+        private readonly ILoggerAdapter<UserService> _logger = Substitute.For<ILoggerAdapter<UserService>>();
 
         public UserServiceTests()
         {
@@ -34,6 +36,58 @@ namespace Users.Api.Tests.Unit
             // Assert
 
             result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnsUsers_WhenSomeUsersExist()
+        {
+            // Arrange
+            var testUser = new User() { 
+                FullName = "TestUser FullName",
+                Id = Guid.NewGuid()
+            };
+            var expectedUsers = new List<User>() { testUser };
+            _userRepository.GetAllAsync().Returns(expectedUsers);
+
+            // Act 
+            var result = await _sut.GetAllAsync();
+            // Assert
+            /// Be vs BeEquivalentTo - reference
+            result.Should().BeEquivalentTo(expectedUsers);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldLogMessages_WhenInvoked()
+        {
+            // Arrange
+            
+            _userRepository.GetAllAsync().Returns(Enumerable.Empty<User>().ToList());
+
+            // Act 
+            await _sut.GetAllAsync();
+            // Assert
+
+            _logger.Received(1).LogInformation(Arg.Is("Retriving all users"));
+            _logger.Received(1).LogInformation(Arg.Is("All users retrieved in {0}ms"), Arg.Any<long>());
+
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldLogMessageAndException_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var exception = new ArgumentException("Something went wrogn while retriving all users");
+
+            _userRepository.GetAllAsync().Throws(exception);
+
+            // Act 
+            var requestAction = async () => await _sut.GetAllAsync();
+            // Assert
+
+            await requestAction.Should().ThrowAsync<ArgumentException>();
+            _logger.Received(1).LogError(Arg.Is(exception), Arg.Is("Something went wrogn while retriving all users"));
+            // _logger.Received(1).LogError(exception, "Something went wrogn while retriving all users");
+
         }
 
     }
